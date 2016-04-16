91399e6bea18da6517789a0d76e0c0f3601b58bb
Raphael Benitte <benitte@ekino.com>
2016-04-16 14:11:50 +0900

    feat(axes): add AxisX component

--- src/components/axes/AxisX.jsx
+++ src/components/axes/AxisX.jsx
@@ -2,17 +2,60 @@ import React, { Component, PropTypes } from 'react';
 import d3                              from 'd3';
 
 class AxisX extends Component {
+    shouldComponentUpdate(nextProps) {
+        const {
+            orient,
+            xScale,
+            tickMode,
+            tickPadding,
+            height,
+            transitionDuration,
+            transitionEasing
+        } = nextProps;
+
+        const element = d3.select(React.findDOMNode(this));
+        element.attr('transform', `translate(0, ${height})`);
+
+        const axis = d3.svg.axis()
+            .scale(xScale)
+            .tickPadding(tickPadding)
+            .orient(orient)
+        ;
+
+        if (tickMode === 'grid') {
+            axis.tickSize(-height);
+        }
+
+        element
+            .transition()
+            .duration(transitionDuration)
+            .ease(transitionEasing)
+            .call(axis)
+        ;
+
+        return false;
+    }
+
     render() {
-        return null;
+        return <g className="chart__axis chart__axis--x"/>;
     }
 }
 
 AxisX.displayName = 'AxisX';
 
 AxisX.propTypes = {
+    orient:      PropTypes.oneOf(['top', 'bottom']).isRequired,
+    xScale:      PropTypes.func.isRequired,
+    tickMode:    PropTypes.oneOf(['normal', 'grid']).isRequired,
+    tickPadding: PropTypes.number.isRequired
 };
 
 AxisX.defaultProps = {
+    orient:             'bottom',
+    transitionDuration: 600,
+    transitionEasing:   'cubic-out',
+    tickMode:           'normal',
+    tickPadding:        3
 };
 
 
