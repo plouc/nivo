08a7259c7b56962058428158e7b379ef3e04d1b4
Raphael Benitte <benitte@ekino.com>
2016-04-21 09:02:39 +0900

    feat(bubble): move Bubble legends in a dedicated component

--- src/components/layouts/Bubble.js
+++ src/components/layouts/Bubble.js
@@ -5,16 +5,15 @@ import Dimensions                             from 'react-dimensions';
 import Nivo                                   from '../../Nivo';
 import { margin as marginPropType }           from '../../PropTypes';
 import { flatten }                            from '../../DataUtils';
-import { getColorStyleObject, getColorRange } from '../../ColorUtils';
+import { getColorRange }                      from '../../ColorUtils';
 
 
 class Bubble extends Component {
     renderD3(nextProps) {
         const {
             root,
-            mode,
-            valueAccessor, labelFn,
             containerWidth, containerHeight,
+            padding,
             colors,
             transitionDuration, transitionEasing
         } = nextProps;
@@ -38,42 +37,34 @@ class Bubble extends Component {
         const bubble = d3.layout.pack()
             .sort(null)
             .size([width, height])
-            .padding(1)
-        ;
-
-        let nodes = wrapper.selectAll('.bubble_node')
-            .data(bubble.nodes(flatten(root))
-            .filter(d => !d.children))
+            .padding(padding)
         ;
 
         const color = getColorRange(colors);
 
+        const flattened = flatten(root);
+        flattened.children.forEach(child => {
+            child.color = color(child.packageName);
+        });
+
+        const bubbled = bubble.nodes(flattened).filter(d => !d.children);
+        const nodes   = wrapper.selectAll('.bubble_node').data(bubbled);
+
         nodes
-            .enter().append('g')
+            .enter().append('circle')
             .attr('class', 'bubble_node')
-            .attr('transform', d => `translate(${d.x},${d.y})`)
-        .append('circle')
             .attr('r', 2)
             .style('fill', d => color(d.packageName))
+            .attr('transform', d => `translate(${d.x},${d.y})`)
         ;
 
         nodes
             .transition()
             .duration(transitionDuration)
             .ease(transitionEasing)
+            .attr('r', d => d.r)
             .attr('transform', d => `translate(${d.x},${d.y})`)
         ;
-
-        nodes.each(function (d) {
-            const el = d3.select(this);
-
-            el.select('circle')
-                .transition()
-                .duration(transitionDuration)
-                .ease(transitionEasing)
-                .attr('r', d => d.r)
-            ;
-        });
 
         nodes.exit()
             .transition()
@@ -82,6 +73,21 @@ class Bubble extends Component {
             .attr('transform', `translate(${width / 2},${height / 2})`)
             .remove()
         ;
+
+        const bubbleContext = {
+            element:  wrapper,
+            width,
+            height,
+            rawData:  root,
+            flatData: flattened,
+            data:     bubbled,
+            transitionDuration,
+            transitionEasing
+        };
+
+        this.legends.forEach(legend => {
+            legend(bubbleContext);
+        });
     }
 
     shouldComponentUpdate(nextProps) {
@@ -95,6 +101,19 @@ class Bubble extends Component {
     }
 
     componentWillMount() {
+        const { children } = this.props;
+
+        const legends = [];
+
+        React.Children.forEach(children, element => {
+            if (React.isValidElement(element)) {
+                if (element.type.createBubbleLegendsFromReactElement) {
+                    legends.push(element.type.createBubbleLegendsFromReactElement(element));
+                }
+            }
+        });
+
+        this.legends = legends;
     }
 
     render() {
@@ -113,8 +132,7 @@ Bubble.propTypes = {
     containerHeight:    number.isRequired,
     margin:             marginPropType,
     root:               object.isRequired,
-    valueAccessor:      func.isRequired,
-    labelFn:            func.isRequired,
+    padding:            number.isRequired,
     colors:             any.isRequired,
     transitionDuration: number.isRequired,
     transitionEasing:   string.isRequired
@@ -122,8 +140,7 @@ Bubble.propTypes = {
 
 Bubble.defaultProps = {
     margin:             Nivo.defaults.margin,
-    valueAccessor:      d => d.size,
-    labelFn:            d => d.name,
+    padding:            1,
     colors:             Nivo.defaults.colorRange,
     transitionDuration: Nivo.defaults.transitionDuration,
     transitionEasing:   Nivo.defaults.transitionEasing
