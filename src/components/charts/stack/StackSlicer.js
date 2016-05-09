fad06c7a7a301820d7d320d794acdefa74585e36
Raphael Benitte <benitte@ekino.com>
2016-05-09 17:31:05 +0900

    feat(stack-slicer): split dot and line related properties

--- src/components/charts/stack/StackSlicer.js
+++ src/components/charts/stack/StackSlicer.js
@@ -23,9 +23,10 @@ class StackSlicer extends Component {
     static decorateStack(element) {
         const { props } = element;
 
-        const colorFn        = getColorGenerator(props.color);
-        const borderColorFn  = getColorGenerator(props.borderColor);
-        const { showOnOver } = props;
+        const colorFn          = getColorGenerator(props.color);
+        const dotBorderColorFn = getColorGenerator(props.dotBorderColor);
+        const lineColorFn      = getColorGenerator(props.lineColor);
+        const { showOnOver }   = props;
 
         // Receive context from Parent Stack component
         return ({
@@ -110,8 +111,8 @@ class StackSlicer extends Component {
             lines.enter().append('line')
                 .attr('y1', d => d.interpolated.y)
                 .attr('y2', d => d.interpolated.y0)
-                .style('stroke-width', props.borderWidth)
-                .style('stroke', borderColorFn)
+                .style('stroke-width', props.lineWidth)
+                .style('stroke', lineColorFn)
             ;
 
             // UPDATE
@@ -121,8 +122,8 @@ class StackSlicer extends Component {
                 .ease(transitionEasing)
                 .attr('y1', d => d.interpolated.y)
                 .attr('y2', d => d.interpolated.y0)
-                .style('stroke-width', props.borderWidth)
-                .style('stroke', borderColorFn)
+                .style('stroke-width', props.lineWidth)
+                .style('stroke', lineColorFn)
             ;
 
             // EXIT
@@ -148,8 +149,8 @@ class StackSlicer extends Component {
                 .style('cursor', 'pointer')
                 .attr('transform', d => `translate(0,${d.interpolated.y})`)
                 .style('fill', colorFn)
-                .style('stroke-width', props.borderWidth)
-                .style('stroke', borderColorFn)
+                .style('stroke-width', props.dotBorderWidth)
+                .style('stroke', dotBorderColorFn)
             ;
 
             // UPDATE
@@ -176,8 +177,8 @@ class StackSlicer extends Component {
                 .attr('r', props.radius)
                 .attr('transform', d => `translate(0,${d.interpolated.y})`)
                 .style('fill', colorFn)
-                .style('stroke-width', props.borderWidth)
-                .style('stroke', borderColorFn)
+                .style('stroke-width', props.dotBorderWidth)
+                .style('stroke', dotBorderColorFn)
             ;
 
             // EXIT
@@ -203,19 +204,22 @@ class StackSlicer extends Component {
 const { number, bool, any } = PropTypes;
 
 StackSlicer.propTypes = {
-    showOnOver:  bool.isRequired,
-    radius:      number.isRequired,
-    color:       any.isRequired,
-    borderWidth: number.isRequired,
-    borderColor: any.isRequired
+    showOnOver:     bool.isRequired,
+    radius:         number.isRequired,
+    color:          any.isRequired,
+    dotBorderWidth: number.isRequired,
+    dotBorderColor: any.isRequired,
+    lineWidth:      number.isRequired,
 };
 
 StackSlicer.defaultProps = {
-    showOnOver:  false,
-    radius:      4,
-    color:       'inherit',
-    borderWidth: 1,
-    borderColor: 'inherit:darker(.4)',
+    showOnOver:     false,
+    radius:         4,
+    color:          'inherit',
+    dotBorderWidth: 1,
+    dotBorderColor: 'inherit:darker(.5)',
+    lineWidth:      1,
+    lineColor:      'inherit:darker(.3)',
 };
 
 
