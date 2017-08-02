b1c8feb1e2919b6f88d19aea6afa866a872f03ab
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-02 13:16:40 +0900

    test(unit): move unit tests to jest & remove karma tests

--- test/ArcUtil.test.js
+++ test/ArcUtil.test.js
@@ -1,16 +1,13 @@
 /*
  * This file is part of the nivo project.
  *
- * (c) Raphaël Benitte
+ * Copyright 2016-present, Raphaël Benitte.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-'use strict'
-
-import test from 'ava'
 import { midAngle } from '../src/ArcUtils'
 
-test('midAngle() should compute center of given angles', t => {
-    t.is(midAngle({ startAngle: 0, endAngle: 90 }), 45)
+test('midAngle() should compute center of given angles', () => {
+    expect(midAngle({ startAngle: 0, endAngle: 90 })).toBe(45)
 })
--- test/ColorUtils.test.js
+++ test/ColorUtils.test.js
@@ -1,64 +1,64 @@
 /*
  * This file is part of the nivo project.
  *
- * (c) Raphaël Benitte
+ * Copyright 2016-present, Raphaël Benitte.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-'use strict'
-
-import test from 'ava'
-import d3 from 'd3'
+import * as d3 from 'd3'
 import { getColorGenerator } from '../src/ColorUtils'
 
-test(`getColorGenerator() should return 'none' if 'none' provided`, t => {
-    t.is(getColorGenerator('none'), 'none')
-})
+describe('getColorGenerator()', () => {
+    it(`should return 'none' if 'none' provided`, () => {
+        expect(getColorGenerator('none')).toBe('none')
+    })
 
-test(`getColorGenerator() should return a function to use 'data.color' if 'inherit' provided`, t => {
-    const colorGenerator = getColorGenerator('inherit')
-    const color = '#FF0000'
+    it(`should return a function to use 'data.color' if 'inherit' provided`, () => {
+        const colorGenerator = getColorGenerator('inherit')
+        const color          = '#FF0000'
 
-    t.is(typeof colorGenerator, 'function')
-    t.is(colorGenerator({ data: { color } }), color)
-})
+        expect(typeof colorGenerator).toBe('function')
+        expect(colorGenerator({data: {color}})).toBe(color)
+    })
 
-test(`getColorGenerator() should return a function to use darker 'data.color' if 'inherit:darker(*)' provided`, t => {
-    const colorGenerator = getColorGenerator('inherit:darker(1)')
-    const color = '#FF0000'
+    it(`should return a function to use darker 'data.color' if 'inherit:darker(*)' provided`, () => {
+        const colorGenerator = getColorGenerator('inherit:darker(1)')
+        const color          = '#FF0000'
 
-    t.is(typeof colorGenerator, 'function')
-    t.deepEqual(colorGenerator({ data: { color } }), d3.rgb(color).darker(1))
-})
+        expect(typeof colorGenerator).toBe('function')
+        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).darker(1))
+    })
 
-test(`getColorGenerator() 'inherit:darker(*)' should support floats`, t => {
-    const colorGenerator = getColorGenerator('inherit:darker(.3)')
-    const color = '#FF0000'
+    it(`'inherit:darker(*)' should support floats`, () => {
+        const colorGenerator = getColorGenerator('inherit:darker(.3)')
+        const color          = '#FF0000'
 
-    t.is(typeof colorGenerator, 'function')
-    t.deepEqual(colorGenerator({ data: { color } }), d3.rgb(color).darker(0.3))
-})
+        expect(typeof colorGenerator).toBe('function')
+        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).darker(0.3))
+    })
 
-test(`getColorGenerator() should return a function to use brighter 'data.color' if 'inherit:brighter(*)' provided`, t => {
-    const colorGenerator = getColorGenerator('inherit:brighter(1)')
-    const color = '#FF0000'
+    it(`should return a function to use brighter 'data.color' if 'inherit:brighter(*)' provided`, () => {
+        const colorGenerator = getColorGenerator('inherit:brighter(1)')
+        const color          = '#FF0000'
 
-    t.is(typeof colorGenerator, 'function')
-    t.deepEqual(colorGenerator({ data: { color } }), d3.rgb(color).brighter(1))
-})
+        expect(typeof colorGenerator).toBe('function')
+        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).brighter(1))
+    })
 
-test(`getColorGenerator() 'inherit:brighter(*)' should support floats`, t => {
-    const colorGenerator = getColorGenerator('inherit:brighter(.3)')
-    const color = '#FF0000'
+    it(`'inherit:brighter(*)' should support floats`, () => {
+        const colorGenerator = getColorGenerator('inherit:brighter(.3)')
+        const color          = '#FF0000'
 
-    t.is(typeof colorGenerator, 'function')
-    t.deepEqual(colorGenerator({ data: { color } }), d3.rgb(color).brighter(0.3))
-})
+        expect(typeof colorGenerator).toBe('function')
+        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).brighter(0.3))
+    })
 
-test(`getColorGenerator() should return directive if no match found`, t => {
-    const color = '#F00'
-    const colorGenerator = getColorGenerator(color)
+    it(`should return directive if no match found`, () => {
+        const color          = '#F00'
+        const colorGenerator = getColorGenerator(color)
 
-    t.is(colorGenerator, color)
+        expect(typeof colorGenerator).toBe('function')
+        expect(colorGenerator()).toBe(color)
+    })
 })
