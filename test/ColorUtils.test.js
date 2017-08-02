cae0b423dc99d98e515be86d7617113c01c00f44
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-02 13:40:39 +0900

    chore(style): add missing formatting

--- test/ColorUtils.test.js
+++ test/ColorUtils.test.js
@@ -16,46 +16,46 @@ describe('getColorGenerator()', () => {
 
     it(`should return a function to use 'data.color' if 'inherit' provided`, () => {
         const colorGenerator = getColorGenerator('inherit')
-        const color          = '#FF0000'
+        const color = '#FF0000'
 
         expect(typeof colorGenerator).toBe('function')
-        expect(colorGenerator({data: {color}})).toBe(color)
+        expect(colorGenerator({ data: { color } })).toBe(color)
     })
 
     it(`should return a function to use darker 'data.color' if 'inherit:darker(*)' provided`, () => {
         const colorGenerator = getColorGenerator('inherit:darker(1)')
-        const color          = '#FF0000'
+        const color = '#FF0000'
 
         expect(typeof colorGenerator).toBe('function')
-        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).darker(1))
+        expect(colorGenerator({ data: { color } })).toEqual(d3.rgb(color).darker(1))
     })
 
     it(`'inherit:darker(*)' should support floats`, () => {
         const colorGenerator = getColorGenerator('inherit:darker(.3)')
-        const color          = '#FF0000'
+        const color = '#FF0000'
 
         expect(typeof colorGenerator).toBe('function')
-        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).darker(0.3))
+        expect(colorGenerator({ data: { color } })).toEqual(d3.rgb(color).darker(0.3))
     })
 
     it(`should return a function to use brighter 'data.color' if 'inherit:brighter(*)' provided`, () => {
         const colorGenerator = getColorGenerator('inherit:brighter(1)')
-        const color          = '#FF0000'
+        const color = '#FF0000'
 
         expect(typeof colorGenerator).toBe('function')
-        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).brighter(1))
+        expect(colorGenerator({ data: { color } })).toEqual(d3.rgb(color).brighter(1))
     })
 
     it(`'inherit:brighter(*)' should support floats`, () => {
         const colorGenerator = getColorGenerator('inherit:brighter(.3)')
-        const color          = '#FF0000'
+        const color = '#FF0000'
 
         expect(typeof colorGenerator).toBe('function')
-        expect(colorGenerator({data: {color}})).toEqual(d3.rgb(color).brighter(0.3))
+        expect(colorGenerator({ data: { color } })).toEqual(d3.rgb(color).brighter(0.3))
     })
 
     it(`should return directive if no match found`, () => {
-        const color          = '#F00'
+        const color = '#F00'
         const colorGenerator = getColorGenerator(color)
 
         expect(typeof colorGenerator).toBe('function')
