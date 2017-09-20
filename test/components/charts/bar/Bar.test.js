570e48c48017048f0c4f0e56e6691a46a17804bc
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-09-21 04:39:29 +0900

    test(bar): init tests on Bar component

---
+++ test/components/charts/bar/Bar.test.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Bar from '../../../../src/components/charts/bar/Bar'
+
+it('should render a basic bar chart', () => {
+    const component = renderer.create(
+        <Bar
+            width={500}
+            height={300}
+            data={[{ id: 'one', value: 10 }, { id: 'two', value: 20 }, { id: 'three', value: 30 }]}
+        />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+})
+
+it('should allow to disable labels', () => {
+    const component = renderer.create(
+        <Bar
+            width={500}
+            height={300}
+            enableLabel={false}
+            data={[{ id: 'one', value: 10 }, { id: 'two', value: 20 }, { id: 'three', value: 30 }]}
+        />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+})
+
+it('should allow grouped mode', () => {
+    const component = renderer.create(
+        <Bar
+            width={500}
+            height={300}
+            enableLabel={false}
+            groupMode="grouped"
+            data={[{ id: 'one', value: 10 }, { id: 'two', value: 20 }, { id: 'three', value: 30 }]}
+        />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+})
+
+it('should allow horizontal layout', () => {
+    const component = renderer.create(
+        <Bar
+            width={500}
+            height={300}
+            enableLabel={false}
+            layout="horizontal"
+            data={[{ id: 'one', value: 10 }, { id: 'two', value: 20 }, { id: 'three', value: 30 }]}
+        />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+})
+
+it('should allow grouped horizontal layout', () => {
+    const component = renderer.create(
+        <Bar
+            width={500}
+            height={300}
+            enableLabel={false}
+            groupMode="grouped"
+            layout="horizontal"
+            data={[{ id: 'one', value: 10 }, { id: 'two', value: 20 }, { id: 'three', value: 30 }]}
+        />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+})
