da71c432155483547b0e6320d124e25830812d74
Raphaël Benitte <raphael.benitte@ekino.com>
2018-06-05 07:07:34 +0900

    feat(calendar): add TypeScript definitions

---
+++ packages/nivo-calendar/index.d.ts
@@ -0,0 +1,78 @@
+import * as React from 'react'
+import { Dimensions, Theme, Box } from '@nivo/core'
+import { LegendProps } from '@nivo/legends'
+
+declare module '@nivo/calendar' {
+    export type DateOrString = string | Date
+
+    export interface CalendarDatum {
+        day: string
+        value: number
+    }
+
+    export interface CalendarData {
+        from: DateOrString
+        to: DateOrString
+        data: CalendarDatum[]
+    }
+
+    export type CalendarDirection = 'horizontal' | 'vertical'
+
+    export type CalendarLegend = LegendProps & {
+        itemCount: number
+    }
+
+    export interface CalendarDayData {
+        date: Date
+        day: string
+        value?: number
+        color: string
+        size: number
+        x: number
+        y: number
+    }
+
+    export type CalendarCommonProps = Partial<{
+        domain: 'auto' | number[]
+        direction: CalendarDirection
+        colors: string[]
+        margin: Box
+
+        // years
+        yearLabel: (year: number) => string | number
+        yearSpacing: number
+        yearLegendOffset: number
+
+        // months
+        monthLabel: (year: number, month: number) => string | number
+        monthBorderWidth: number
+        monthBorderColor: string
+        monthLegendOffset: number
+
+        // days
+        daySpacing: number
+        dayBorderWidth: number
+        dayBorderColor: string
+        emptyColor: string
+
+        // interactivity
+        isInteractive: boolean
+
+        // tooltip
+        tooltipFormat: (value: number) => string | number
+
+        // legends
+        legends: CalendarLegend[]
+
+        theme: Theme
+    }>
+
+    export type CalendarSvgProps = CalendarData
+        & CalendarCommonProps
+        & Partial<{
+            onClick: (datum: CalendarDayData, event: React.MouseEvent<SVGRectElement>) => void
+        }>
+
+    export class Calendar extends React.Component<CalendarSvgProps & Dimensions> {}
+    export class ResponsiveCalendar extends React.Component<CalendarSvgProps> {}
+}
