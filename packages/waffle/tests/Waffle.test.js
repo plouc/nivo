87e3d9ec3f7b818484530f62abaca8914bc58813
Raphaël Benitte <raphael.benitte@ekino.com>
2018-08-26 09:09:40 +0900

    tests(waffle): add test for Waffle legends

--- packages/waffle/tests/Waffle.test.js
+++ packages/waffle/tests/Waffle.test.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
+import { LegendSvg, LegendSvgItem } from '@nivo/legends'
 import Waffle from '../src/Waffle'
 
 it('should render a basic waffle chart in SVG', () => {
@@ -37,3 +39,53 @@ for (const fillMode of fillModes) {
         expect(tree).toMatchSnapshot()
     })
 }
+
+describe('legends', () => {
+    it('should support legends', () => {
+        const data = [
+            { id: 'one', label: 'one', value: 10 },
+            { id: 'two', label: 'two', value: 20 },
+            { id: 'tree', label: 'tree', value: 30 },
+        ]
+        const legends = [
+            {
+                anchor: 'top-left',
+                direction: 'column',
+                itemWidth: 100,
+                itemHeight: 20,
+            },
+        ]
+        const wrapper = mount(
+            <Waffle
+                width={400}
+                height={400}
+                rows={10}
+                columns={10}
+                total={100}
+                colors={['red', 'green', 'blue']}
+                data={data}
+                legends={legends}
+            />
+        )
+
+        expect(wrapper.find(LegendSvg)).toHaveLength(1)
+
+        const legendItems = wrapper.find(LegendSvgItem)
+        expect(legendItems).toHaveLength(3)
+        expect(legendItems.at(0).prop('data')).toEqual({
+            id: 'one',
+            label: 'one',
+            color: 'red',
+        })
+        expect(legendItems.at(1).prop('data')).toEqual({
+            id: 'two',
+            label: 'two',
+            color: 'green',
+        })
+        expect(legendItems.at(2).prop('data')).toEqual({
+            id: 'tree',
+            label: 'tree',
+            color: 'blue',
+        })
+    })
+})
