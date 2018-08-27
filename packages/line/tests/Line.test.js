1dc93d6388519073b5b3d8923098123b72052788
Raphaël Benitte <raphael.benitte@ekino.com>
2018-08-27 13:12:09 +0900

    tests(line): add test for curve interpolation

--- packages/line/tests/Line.test.js
+++ packages/line/tests/Line.test.js
@@ -67,3 +67,47 @@ it('should have left and bottom axis by default', () => {
     expect(axes.at(0).prop('position')).toBe('bottom')
     expect(axes.at(1).prop('position')).toBe('left')
 })
+
+describe('curve interpolation', () => {
+    const curveInterpolations = [
+        'basis',
+        'cardinal',
+        'catmullRom',
+        'linear',
+        'monotoneX',
+        'monotoneY',
+        'natural',
+        'step',
+        'stepAfter',
+        'stepBefore',
+    ]
+    const data = [
+        {
+            id: 'default',
+            data: [{ x: 0, y: 3 }, { x: 1, y: 7 }, { x: 2, y: 11 }, { x: 3, y: 9 }, { x: 4, y: 8 }],
+        },
+    ]
+    for (const curveInterpolation of curveInterpolations) {
+        it(`should support ${curveInterpolation} curve interpolation`, () => {
+            const component = renderer.create(
+                <Line
+                    width={500}
+                    height={300}
+                    data={data}
+                    curve={curveInterpolation}
+                    axisLeft={undefined}
+                    axisBottom={undefined}
+                    isInteractive={false}
+                    enableStackTooltip={false}
+                    animate={false}
+                    enableDots={false}
+                    enableGridX={false}
+                    enableGridY={false}
+                />
+            )
+
+            const tree = component.toJSON()
+            expect(tree).toMatchSnapshot()
+        })
+    }
+})
