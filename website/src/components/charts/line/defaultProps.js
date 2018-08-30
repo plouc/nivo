70d00527d69b97cb1c30f588212ba2a4da752127
Raphaël Benitte <raphael.benitte@ekino.com>
2018-08-30 10:27:34 +0900

    feat(website): change line demo data generation method

--- website/src/components/charts/line/defaultProps.js
+++ website/src/components/charts/line/defaultProps.js
@@ -59,7 +59,7 @@ export default {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: 'country code',
+        legend: 'transportation',
         legendOffset: 36,
         legendPosition: 'center',
     },
