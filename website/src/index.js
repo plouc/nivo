7808b736c770ea208f26b711334ad8ecc6b3035c
meganhng <meganhong17@gmail.com>
2019-02-20 08:28:21 -0500

    Fixed formatting by removing semi colon from import line

--- website/src/index.js
+++ website/src/index.js
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import 'babel-polyfill';
+import ' babel-polyfill'
 import './styles/index.css'
 import './polyfills'
 import React, { Component } from 'react'
