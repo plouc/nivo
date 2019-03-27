addf67f567d35e9172a23af2e2fc8ec384552f6a
Raphaël Benitte <raphael.benitte@ekino.com>
2019-03-28 00:26:37 +0900

    Updates

---
+++ storybook/precache-manifest.164c5504db3db8a853303d1b39c833d8.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.701fcf6a6d716f169e16.bundle.js"
+  },
+  {
+    "url": "runtime~main.701fcf6a6d716f169e16.bundle.js"
+  },
+  {
+    "url": "main.701fcf6a6d716f169e16.bundle.js"
+  },
+  {
+    "revision": "872ee00f99107510c2cb9d040b788e67",
+    "url": "iframe.html"
+  }
+];
