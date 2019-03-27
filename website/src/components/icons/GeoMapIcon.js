8426ef5b78f4111d04cfeb73729d7ad69722e7f9
Raphaël Benitte <raphael.benitte@ekino.com>
2019-03-27 20:38:14 +0900

    feat(website): udpate geo icons

--- website/src/components/icons/GeoMapIcon.js
+++ website/src/components/icons/GeoMapIcon.js
@@ -6,76 +6,31 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import React, { Fragment } from 'react'
-import { GeoMap } from '@nivo/geo'
-import {} from 'd3-geo'
+import React from 'react'
 import geomapGreyImg from '../../assets/icons/geomap-grey.png'
 import geomapRedImg from '../../assets/icons/geomap-red.png'
-import { ICON_SIZE, Icon } from './styled'
-import world from '../charts/geo/world_countries'
-
-const includeCountries = ['DZA', 'NER', 'BFA', 'MLI', 'MRT']
-const features = world.features.filter(f => includeCountries.includes(f.id))
-
-const chartProps = {
-    width: ICON_SIZE,
-    height: ICON_SIZE,
-    features,
-    projectionScale: 166,
-    projectionTranslation: [0.8, 1.2],
-    isInteractive: false,
-    borderWidth: 1,
-}
+import { Icon } from './styled'
+import MapIcon from './MapIcon'
 
 const GeoMapIcon = () => (
-    <Fragment>
-        <Icon
-            id="geomap-grey"
-            style={{
-                position: 'relative',
-            }}
-        >
-            <GeoMap {...chartProps} borderWidth={6} borderColor="#686868" />
-            <div
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    zIndex: 10,
-                }}
-            >
-                <GeoMap {...chartProps} fillColor="#e2e2e2" borderColor="#686868" />
-            </div>
+    <>
+        <Icon id="geomap-grey">
+            <MapIcon stroke="#686868" fill="#e2e2e2" />
         </Icon>
         <Icon
             style={{
                 backgroundImage: `url(${geomapGreyImg})`,
             }}
         />
-        <Icon
-            id="geomap-red"
-            style={{
-                position: 'relative',
-            }}
-        >
-            <GeoMap {...chartProps} borderWidth={6} borderColor="#d45036" />
-            <div
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    zIndex: 10,
-                }}
-            >
-                <GeoMap {...chartProps} fillColor="#ffc6c5" borderColor="#d45036" />
-            </div>
+        <Icon id="geomap-red">
+            <MapIcon stroke="#d45036" fill="#ffc6c4" />
         </Icon>
         <Icon
             style={{
                 backgroundImage: `url(${geomapRedImg})`,
             }}
         />
-    </Fragment>
+    </>
 )
 
 export default GeoMapIcon
--- website/src/components/icons/Icons.js
+++ website/src/components/icons/Icons.js
@@ -24,6 +24,7 @@ import ParallelCoordinatesIcon from './ParallelCoordinatesIcon'
 
 const Icons = () => (
     <Container>
+        <GeoMapIcon />
         <ChoroplethIcon />
         <ParallelCoordinatesIcon />
         <ScatterPlotIcon />
@@ -36,7 +37,6 @@ const Icons = () => (
         <BarIcon />
         <HeatMapIcon />
         <WaffleIcon />
-        <GeoMapIcon />
     </Container>
 )
 
