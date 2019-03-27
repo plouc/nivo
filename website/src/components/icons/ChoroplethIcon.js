963c821e5892b856ced6d8e6699951ea3d9d2f23
Raphaël Benitte <raphael.benitte@ekino.com>
2019-03-27 20:38:14 +0900

    feat(website): udpate geo icons

--- website/src/components/icons/ChoroplethIcon.js
+++ website/src/components/icons/ChoroplethIcon.js
@@ -7,92 +7,66 @@
  * file that was distributed with this source code.
  */
 import React, { Fragment } from 'react'
-import { Choropleth } from '@nivo/geo'
 import choroplethGreyImg from '../../assets/icons/choropleth-grey.png'
 import choroplethRedImg from '../../assets/icons/choropleth-red.png'
-import { ICON_SIZE, Icon } from './styled'
-import world from '../charts/geo/world_countries'
-
-const includeCountries = ['DZA', 'NER', 'BFA', 'MLI', 'MRT']
-const features = world.features.filter(f => includeCountries.includes(f.id))
-
-const data = [
-    { id: 'DZA', value: 3 },
-    { id: 'NER', value: 6 },
-    { id: 'BFA', value: 1 },
-    { id: 'MLI', value: 12 },
-    { id: 'MRT', value: 7 },
-    { id: 'COD', value: 9 },
-]
-
-const chartProps = {
-    width: ICON_SIZE,
-    height: ICON_SIZE,
-    features,
-    data,
-    projectionScale: 166,
-    projectionTranslation: [0.8, 1.2],
-    isInteractive: false,
-    borderWidth: 1,
-}
+import { Icon } from './styled'
+import MapIcon from './MapIcon'
 
 const ChoroplethIcon = () => (
-    <Fragment>
-        <Icon
-            id="choropleth-grey"
-            style={{
-                position: 'relative',
-            }}
-        >
-            <Choropleth {...chartProps} borderColor="#686868" borderWidth={6} />
-            <div
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    zIndex: 10,
-                }}
-            >
-                <Choropleth
-                    {...chartProps}
-                    borderWidth={0}
-                    colors={['#ddd', '#bbb', '#999', '#777']}
-                />
-            </div>
+    <>
+        <Icon id="choropleth-grey">
+            <MapIcon
+                stroke="#686868"
+                fill="#e2e2e2"
+                colors={[
+                    '#b0aeaf',
+                    '#c3c3c3',
+                    '#e2e2e2',
+                    '#909090',
+                    '#e2e2e2',
+                    '#b0aeaf',
+                    '#c3c3c3',
+                    '#adadad',
+                    '#909090',
+                    '#e2e2e2',
+                    '#b0aeaf',
+                    '#c3c3c3',
+                    '#adadad',
+                ]}
+            />
         </Icon>
         <Icon
             style={{
                 backgroundImage: `url(${choroplethGreyImg})`,
             }}
         />
-        <Icon
-            id="choropleth-red"
-            style={{
-                position: 'relative',
-            }}
-        >
-            <Choropleth {...chartProps} borderColor="#d45036" borderWidth={6} />
-            <div
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    zIndex: 10,
-                }}
-            >
-                <Choropleth
-                    {...chartProps}
-                    borderWidth={0}
-                    colors={['#ffb8b4', '#ff8b7d', '#ff5c3e']}
-                />
-            </div>
+        <Icon id="choropleth-red">
+            <MapIcon
+                stroke="#d45036"
+                fill="#ffc6c4"
+                colors={[
+                    '#ff5c3e',
+                    '#ff8c7d',
+                    '#ffc6c4',
+                    '#ff6b5a',
+                    '#ffc6c4',
+                    '#ff5c3e',
+                    '#ff8c7d',
+                    '#ff8c7d',
+                    '#ff6b5a',
+                    '#ffc6c4',
+                    '#ff5c3e',
+                    '#ff8c7d',
+                    '#ff8c7d',
+                ]}
+            />
         </Icon>
         <Icon
             style={{
                 backgroundImage: `url(${choroplethRedImg})`,
             }}
         />
-    </Fragment>
+    </>
 )
 
 export default ChoroplethIcon
