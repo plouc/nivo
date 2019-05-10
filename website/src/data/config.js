5c866ce7f1e2c41d6bd303debe4ef631c07c0957
Raphaël Benitte <raphael.benitte@ekino.com>
2019-05-10 09:38:50 +0900

    fix(website): fix storybook url config

--- website/src/data/config.js
+++ website/src/data/config.js
@@ -9,9 +9,9 @@
 export default {
     // local
     // nivoApiUrl: 'http://localhost:3030',
-    storybookUrl: 'http://localhost:6006/',
+    // storybookUrl: 'http://localhost:6006/',
 
     // production
     nivoApiUrl: 'https://nivo-api.herokuapp.com',
-    // storybookUrl: 'https://nivo.rocks/storybook/',
+    storybookUrl: 'https://nivo.rocks/storybook/',
 }
