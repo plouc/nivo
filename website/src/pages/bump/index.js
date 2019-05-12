5501852db649210e6c76edb804b5fdcc00ee7b01
Raphaël Benitte <raphael.benitte@ekino.com>
2019-05-12 11:16:24 +0900

    feat(bump): init @nivo/bump package

---
+++ website/src/pages/bump/index.js
@@ -0,0 +1,105 @@
+/*
+ * This file is part of the nivo project.
+ *
+ * Copyright 2016-present, Raphaël Benitte.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+import React from 'react'
+import range from 'lodash/range'
+import shuffle from 'lodash/shuffle'
+import { ResponsiveBump, BumpDefaultProps } from '@nivo/bump'
+import ComponentTemplate from '../../components/components/ComponentTemplate'
+import meta from '../../data/components/bump/meta.yml'
+import { groups } from '../../data/components/bump/props'
+
+const generateData = () => {
+    const years = range(2000, 2005)
+    const ranks = range(1, 13)
+
+    const series = ranks.map(rank => {
+        return {
+            id: `Serie ${rank}`,
+            data: [],
+        }
+    })
+
+    years.forEach(year => {
+        shuffle(ranks).forEach((rank, i) => {
+            series[i].data.push({
+                x: year,
+                y: rank,
+            })
+        })
+    })
+
+    return series
+}
+
+const initialProperties = {
+    margin: {
+        top: 40,
+        right: 100,
+        bottom: 40,
+        left: 60,
+    },
+
+    lineCurvaturePadding: 0.35,
+
+    xOuterPadding: BumpDefaultProps.xOuterPadding,
+    yOuterPadding: BumpDefaultProps.yOuterPadding,
+
+    lineWidth: 3,
+    activeLineWidth: 6,
+    inactiveLineWidth: 1,
+
+    startLabelPadding: BumpDefaultProps.startLabelPadding,
+    startLabelTextColor: BumpDefaultProps.startLabelTextColor,
+    endLabel: 'id',
+    endLabelPadding: BumpDefaultProps.endLabelPadding,
+    endLabelTextColor: BumpDefaultProps.endLabelTextColor,
+
+    colors: { scheme: 'spectral' },
+
+    pointSize: 10,
+    activePointSize: 16,
+    inactivePointSize: 0,
+    pointColor: { from: 'serie.color' },
+
+    enableGridX: true,
+    enableGridY: true,
+    axisLeft: {
+        format: v => `#${v}`,
+        legend: 'ranking',
+        legendPosition: 'middle',
+        legendOffset: -40,
+    },
+
+    isInteractive: true,
+
+    animate: BumpDefaultProps.animate,
+    motionStiffness: BumpDefaultProps.motionStiffness,
+    motionDamping: BumpDefaultProps.motionDamping,
+}
+
+const Bump = () => {
+    return (
+        <ComponentTemplate
+            name="Bump"
+            meta={meta.Bump}
+            icon="chord"
+            flavors={meta.flavors}
+            currentFlavor="svg"
+            properties={groups}
+            initialProperties={initialProperties}
+            generateData={generateData}
+        >
+            {(properties, data, theme, logAction) => {
+                return <ResponsiveBump data={data} {...properties} theme={theme} />
+            }}
+        </ComponentTemplate>
+    )
+}
+
+export default Bump
