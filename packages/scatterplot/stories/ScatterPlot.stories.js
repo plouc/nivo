a0dce29103c647669c395057e64a8de9b4efb0bf
Raphaël Benitte <raphael.benitte@ekino.com>
2019-06-02 11:39:33 +0900

    feat(scatterplot): adapt ScatterPlot stories

--- packages/scatterplot/stories/ScatterPlot.stories.js
+++ packages/scatterplot/stories/ScatterPlot.stories.js
@@ -8,115 +8,115 @@ const sampleData = [
     {
         id: 'girls low',
         data: [
-            { id: 0, x: 0, y: 2.0 },
-            { id: 1, x: 1, y: 2.1 },
-            { id: 2, x: 2, y: 2.3 },
-            { id: 3, x: 3, y: 2.5 },
-            { id: 4, x: 4, y: 2.7 },
-            { id: 5, x: 5, y: 2.9 },
-            { id: 6, x: 6, y: 3.0 },
-            { id: 7, x: 7, y: 3.2 },
-            { id: 8, x: 8, y: 3.3 },
-            { id: 9, x: 9, y: 3.5 },
-            { id: 10, x: 10, y: 3.6 },
-            { id: 11, x: 11, y: 3.8 },
-            { id: 12, x: 12, y: 3.9 },
-            { id: 13, x: 13, y: 4.0 },
+            { id: 0, x: 0, y: 2.0, z: 1 },
+            { id: 1, x: 1, y: 2.1, z: 2 },
+            { id: 2, x: 2, y: 2.3, z: 4 },
+            { id: 3, x: 3, y: 2.5, z: 1 },
+            { id: 4, x: 4, y: 2.7, z: 3 },
+            { id: 5, x: 5, y: 2.9, z: 0 },
+            { id: 6, x: 6, y: 3.0, z: 1 },
+            { id: 7, x: 7, y: 3.2, z: 3 },
+            { id: 8, x: 8, y: 3.3, z: 2 },
+            { id: 9, x: 9, y: 3.5, z: 3 },
+            { id: 10, x: 10, y: 3.6, z: 0 },
+            { id: 11, x: 11, y: 3.8, z: 0 },
+            { id: 12, x: 12, y: 3.9, z: 4 },
+            { id: 13, x: 13, y: 4.0, z: 1 },
         ],
     },
     {
         id: 'girls med',
         data: [
-            { id: 0, x: 0, y: 3.2 },
-            { id: 1, x: 1, y: 3.3 },
-            { id: 2, x: 2, y: 3.6 },
-            { id: 3, x: 3, y: 3.8 },
-            { id: 4, x: 4, y: 4.1 },
-            { id: 5, x: 5, y: 4.3 },
-            { id: 6, x: 6, y: 4.6 },
-            { id: 7, x: 7, y: 4.8 },
-            { id: 8, x: 8, y: 5.0 },
-            { id: 9, x: 9, y: 5.2 },
-            { id: 10, x: 10, y: 5.4 },
-            { id: 11, x: 11, y: 5.5 },
-            { id: 12, x: 12, y: 5.7 },
-            { id: 13, x: 13, y: 5.8 },
+            { id: 0, x: 0, y: 3.2, z: 1 },
+            { id: 1, x: 1, y: 3.3, z: 3 },
+            { id: 2, x: 2, y: 3.6, z: 4 },
+            { id: 3, x: 3, y: 3.8, z: 0 },
+            { id: 4, x: 4, y: 4.1, z: 1 },
+            { id: 5, x: 5, y: 4.3, z: 1 },
+            { id: 6, x: 6, y: 4.6, z: 2 },
+            { id: 7, x: 7, y: 4.8, z: 2 },
+            { id: 8, x: 8, y: 5.0, z: 3 },
+            { id: 9, x: 9, y: 5.2, z: 0 },
+            { id: 10, x: 10, y: 5.4, z: 1 },
+            { id: 11, x: 11, y: 5.5, z: 1 },
+            { id: 12, x: 12, y: 5.7, z: 2 },
+            { id: 13, x: 13, y: 5.8, z: 4 },
         ],
     },
     {
         id: 'girls high',
         data: [
-            { id: 0, x: 0, y: 4.8 },
-            { id: 1, x: 1, y: 5.1 },
-            { id: 2, x: 2, y: 5.4 },
-            { id: 3, x: 3, y: 5.7 },
-            { id: 4, x: 4, y: 6.1 },
-            { id: 5, x: 5, y: 6.5 },
-            { id: 6, x: 6, y: 6.8 },
-            { id: 7, x: 7, y: 7.1 },
-            { id: 8, x: 8, y: 7.3 },
-            { id: 9, x: 9, y: 7.6 },
-            { id: 10, x: 10, y: 7.8 },
-            { id: 11, x: 11, y: 8.1 },
-            { id: 12, x: 12, y: 8.3 },
-            { id: 13, x: 13, y: 8.5 },
+            { id: 0, x: 0, y: 4.8, z: 4 },
+            { id: 1, x: 1, y: 5.1, z: 3 },
+            { id: 2, x: 2, y: 5.4, z: 4 },
+            { id: 3, x: 3, y: 5.7, z: 1 },
+            { id: 4, x: 4, y: 6.1, z: 0 },
+            { id: 5, x: 5, y: 6.5, z: 1 },
+            { id: 6, x: 6, y: 6.8, z: 2 },
+            { id: 7, x: 7, y: 7.1, z: 3 },
+            { id: 8, x: 8, y: 7.3, z: 3 },
+            { id: 9, x: 9, y: 7.6, z: 1 },
+            { id: 10, x: 10, y: 7.8, z: 1 },
+            { id: 11, x: 11, y: 8.1, z: 0 },
+            { id: 12, x: 12, y: 8.3, z: 4 },
+            { id: 13, x: 13, y: 8.5, z: 1 },
         ],
     },
     {
         id: 'boys low',
         data: [
-            { id: 0, x: 0, y: 2.5 },
-            { id: 1, x: 1, y: 2.6 },
-            { id: 2, x: 2, y: 2.8 },
-            { id: 3, x: 3, y: 3.1 },
-            { id: 4, x: 4, y: 3.3 },
-            { id: 5, x: 5, y: 3.5 },
-            { id: 6, x: 6, y: 3.8 },
-            { id: 7, x: 7, y: 4.0 },
-            { id: 8, x: 8, y: 4.2 },
-            { id: 9, x: 9, y: 4.4 },
-            { id: 10, x: 10, y: 4.5 },
-            { id: 11, x: 11, y: 4.7 },
-            { id: 12, x: 12, y: 4.9 },
-            { id: 13, x: 13, y: 5.0 },
+            { id: 0, x: 0, y: 2.5, z: 3 },
+            { id: 1, x: 1, y: 2.6, z: 2 },
+            { id: 2, x: 2, y: 2.8, z: 0 },
+            { id: 3, x: 3, y: 3.1, z: 0 },
+            { id: 4, x: 4, y: 3.3, z: 1 },
+            { id: 5, x: 5, y: 3.5, z: 4 },
+            { id: 6, x: 6, y: 3.8, z: 4 },
+            { id: 7, x: 7, y: 4.0, z: 4 },
+            { id: 8, x: 8, y: 4.2, z: 3 },
+            { id: 9, x: 9, y: 4.4, z: 2 },
+            { id: 10, x: 10, y: 4.5, z: 1 },
+            { id: 11, x: 11, y: 4.7, z: 0 },
+            { id: 12, x: 12, y: 4.9, z: 0 },
+            { id: 13, x: 13, y: 5.0, z: 3 },
         ],
     },
     {
         id: 'boys med',
         data: [
-            { id: 0, x: 0, y: 3.3 },
-            { id: 1, x: 1, y: 3.5 },
-            { id: 2, x: 2, y: 3.8 },
-            { id: 3, x: 3, y: 4.1 },
-            { id: 4, x: 4, y: 4.4 },
-            { id: 5, x: 5, y: 4.7 },
-            { id: 6, x: 6, y: 4.9 },
-            { id: 7, x: 7, y: 5.2 },
-            { id: 8, x: 8, y: 5.4 },
-            { id: 9, x: 9, y: 5.6 },
-            { id: 10, x: 10, y: 5.8 },
-            { id: 11, x: 11, y: 6.0 },
-            { id: 12, x: 12, y: 6.2 },
-            { id: 13, x: 13, y: 6.4 },
+            { id: 0, x: 0, y: 3.3, z: 0 },
+            { id: 1, x: 1, y: 3.5, z: 4 },
+            { id: 2, x: 2, y: 3.8, z: 3 },
+            { id: 3, x: 3, y: 4.1, z: 3 },
+            { id: 4, x: 4, y: 4.4, z: 2 },
+            { id: 5, x: 5, y: 4.7, z: 2 },
+            { id: 6, x: 6, y: 4.9, z: 2 },
+            { id: 7, x: 7, y: 5.2, z: 1 },
+            { id: 8, x: 8, y: 5.4, z: 1 },
+            { id: 9, x: 9, y: 5.6, z: 0 },
+            { id: 10, x: 10, y: 5.8, z: 3 },
+            { id: 11, x: 11, y: 6.0, z: 4 },
+            { id: 12, x: 12, y: 6.2, z: 2 },
+            { id: 13, x: 13, y: 6.4, z: 1 },
         ],
     },
     {
         id: 'boys high',
         data: [
-            { id: 0, x: 0, y: 5.0 },
-            { id: 1, x: 1, y: 5.3 },
-            { id: 2, x: 2, y: 5.6 },
-            { id: 3, x: 3, y: 6.0 },
-            { id: 4, x: 4, y: 6.4 },
-            { id: 5, x: 5, y: 6.8 },
-            { id: 6, x: 6, y: 7.2 },
-            { id: 7, x: 7, y: 7.5 },
-            { id: 8, x: 8, y: 7.8 },
-            { id: 9, x: 9, y: 8.0 },
-            { id: 10, x: 10, y: 8.3 },
-            { id: 11, x: 11, y: 8.5 },
-            { id: 12, x: 12, y: 8.8 },
-            { id: 13, x: 13, y: 9.0 },
+            { id: 0, x: 0, y: 5.0, z: 4 },
+            { id: 1, x: 1, y: 5.3, z: 3 },
+            { id: 2, x: 2, y: 5.6, z: 2 },
+            { id: 3, x: 3, y: 6.0, z: 2 },
+            { id: 4, x: 4, y: 6.4, z: 1 },
+            { id: 5, x: 5, y: 6.8, z: 2 },
+            { id: 6, x: 6, y: 7.2, z: 1 },
+            { id: 7, x: 7, y: 7.5, z: 3 },
+            { id: 8, x: 8, y: 7.8, z: 4 },
+            { id: 9, x: 9, y: 8.0, z: 1 },
+            { id: 10, x: 10, y: 8.3, z: 0 },
+            { id: 11, x: 11, y: 8.5, z: 2 },
+            { id: 12, x: 12, y: 8.8, z: 1 },
+            { id: 13, x: 13, y: 9.0, z: 4 },
         ],
     },
 ]
@@ -125,7 +125,10 @@ const commonProps = {
     width: 900,
     height: 500,
     margin: { top: 24, right: 24, bottom: 80, left: 80 },
-    symbolSize: 10,
+    nodeSize: 10,
+    blendMode: 'multiply',
+    xFormat: d => `week ${d}`,
+    yFormat: d => `${d} kg`,
     axisBottom: {
         format: d => `week ${d}`,
     },
@@ -192,6 +195,7 @@ stories.add('using time scales', () => (
             format: '%Y-%m-%d',
             precision: 'day',
         }}
+        xFormat="time:%Y-%m-%d"
         axisBottom={{
             format: '%b %d',
             tickValues: 'every 2 days',
@@ -219,10 +223,12 @@ stories.add('using logarithmic scales', () => (
             type: 'log',
             base: 10,
         }}
+        xFormat={undefined}
         yScale={{
             type: 'log',
             base: 2,
         }}
+        yFormat={undefined}
         axisBottom={{
             tickValues: [10, 100, 1000, 1000, 10000, 100000, 1000000, 10000000],
         }}
@@ -232,30 +238,32 @@ stories.add('using logarithmic scales', () => (
     />
 ))
 
-stories.add('symbol size', () => <ScatterPlot {...commonProps} symbolSize={24} />)
+stories.add('node size', () => <ScatterPlot {...commonProps} nodeSize={24} />)
 
-stories.add('varying symbol size', () => (
-    <ScatterPlot {...commonProps} symbolSize={d => d.x + d.y * 2} />
+stories.add('varying node size', () => (
+    <ScatterPlot {...commonProps} nodeSize={{ key: 'z', values: [0, 4], sizes: [9, 32] }} />
 ))
 
 stories.add('custom tooltip', () => (
     <ScatterPlot
         {...commonProps}
-        tooltip={({ id, serie, x, y, color }) => (
-            <span style={{ color }}>
-                <strong>
-                    {id} ({serie.id})
-                </strong>
-                : {`x: ${x}, y: ${y}`}
-            </span>
-        )}
-        theme={{
-            tooltip: {
-                container: {
+        tooltip={({ node }) => (
+            <div
+                style={{
+                    color: node.style.color,
                     background: '#333',
-                },
-            },
-        }}
+                    padding: '12px 16px',
+                }}
+            >
+                <strong>
+                    {node.id} ({node.serieId})
+                </strong>
+                <br />
+                {`x: ${node.data.formattedX}`}
+                <br />
+                {`y: ${node.data.formattedY}`}
+            </div>
+        )}
     />
 ))
 
@@ -378,14 +386,14 @@ stories.add('using mouse enter/leave', () => (
     />
 ))
 
-const AreaLayer = ({ points, xScale, yScale }) => {
+const AreaLayer = ({ nodes, xScale, yScale }) => {
     const areaGenerator = area()
         .x(d => xScale(d.data.x))
         .y0(d => yScale(d.data.low))
         .y1(d => yScale(d.data.high))
         .curve(curveMonotoneX)
 
-    return <path d={areaGenerator(points)} fill="rgba(232, 193, 160, .65)" />
+    return <path d={areaGenerator(nodes)} fill="rgba(232, 193, 160, .65)" />
 }
 
 stories.add(
@@ -419,7 +427,7 @@ stories.add(
                 max: 10,
             }}
             legends={[]}
-            layers={['grid', 'axes', AreaLayer, 'points', 'markers', 'mesh', 'legends']}
+            layers={['grid', 'axes', AreaLayer, 'nodes', 'markers', 'mesh', 'legends']}
         />
     ),
     {
