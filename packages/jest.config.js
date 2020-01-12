327498291f686109a5d1964aea9bdfa451edd87a
Raphaël Benitte <benitteraphael@gmail.com>
2020-01-13 08:14:05 +0900

    fix(tests): allow regular js files along with ts ones

--- packages/jest.config.js
+++ packages/jest.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/js-with-ts',
     verbose: true,
     testEnvironment: 'jsdom',
     setupFiles: [path.resolve(path.join(__dirname, 'jest.setup.js'))],
