1f11664152bf74cff5114e5c25d548fd4b794904
plouc <benitteraphael@gmail.com>
2020-06-21 10:43:03 +0900

    feat(build): generate source maps for packages

--- conf/rollup.config.js
+++ conf/rollup.config.js
@@ -57,9 +57,10 @@ const configs = [
     {
         ...common,
         output: {
-            file: `./packages/${pkg}/dist/nivo-${pkg}.esm.js`,
-            format: 'esm',
+            file: `./packages/${pkg}/dist/nivo-${pkg}.es.js`,
+            format: 'es',
             name: `@nivo/${pkg}`,
+            sourcemap: true,
         },
         plugins: commonPlugins,
     }
@@ -72,6 +73,7 @@ if (!isWatching) {
             file: `./packages/${pkg}/dist/nivo-${pkg}.cjs.js`,
             format: 'cjs',
             name: `@nivo/${pkg}`,
+            sourcemap: true,
         },
         plugins: commonPlugins,
     })
@@ -83,6 +85,7 @@ if (!isWatching) {
             extend: true,
             name: 'nivo',
             globals: mapGlobal,
+            sourcemap: true,
         },
         plugins: commonPlugins,
     })
