afa227364aa9c03cb59296e6f16d13c9e7ec69a7
Neil Kistner <neil.kistner@gmail.com>
2020-06-22 09:59:34 -0500

    feat(website): Add daySpacing to calendar component

--- website/src/pages/calendar/index.js
+++ website/src/pages/calendar/index.js
@@ -44,6 +44,7 @@ const initialProperties = {
     yearLegendPosition: 'before',
     yearLegendOffset: 10,
 
+    monthSpacing: 0,
     monthBorderWidth: 2,
     monthBorderColor: '#ffffff',
     monthLegendPosition: 'before',
