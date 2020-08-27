e72bfa37f9425d1531ee4728d935ebd57a3e80e2
Gaurav Shah <gauravshah_786@yahoo.com>
2020-08-27 09:59:41 -0700

    Update GeoMapFeature.js
    Add id to path for GeoMapFeature to access a particular country path

--- packages/geo/src/GeoMapFeature.js
+++ packages/geo/src/GeoMapFeature.js
@@ -24,6 +24,7 @@ const GeoMapFeature = memo(
         return (
             <path
                 key={feature.id}
+                id={feature.id}
                 fill={fillColor}
                 strokeWidth={borderWidth}
                 stroke={borderColor}
