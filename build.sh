cf1961ca8eec412ab74fadeb8a40cbc9183fc62c
Angelica Miranda <angelica@bitbloom.tech>
2020-10-12 15:22:59 -0500

    Test git add

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ npm install yarn
 make init
 git config --global user.name "lerna-bot"
 git config --global user.email "lerna-bot@lbot.io"
+git commit -a -m 'Build'
 git branch tmp
 
 make packages-publish
