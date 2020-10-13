6ea3672d9aef34658a3a21ef1a7883fc8a367deb
Angelica Miranda <angelica@bitbloom.tech>
2020-10-13 13:33:29 -0500

    Add dummy lerna credentials

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -e
 
 npm install yarn
 make init
+git config --global user.name "lerna-bot"
+git config --global user.email "lerna-bot@lbot.io"
 git commit -a -m 'Build'
 
 make packages-publish
