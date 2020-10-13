8c1d2aa461d861e8ce5476f26c7d8611ab4d13e8
Angelica Miranda <angelica@bitbloom.tech>
2020-10-13 12:36:17 -0500

    Allow lerna push & no canary

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -e
 
 npm install yarn
 make init
-git config --global user.name "lerna-bot"
-git config --global user.email "lerna-bot@lbot.io"
 git commit -a -m 'Build'
 
 make packages-publish
-
-#make packages-publish
