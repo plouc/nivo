581ff4246f97f16ac669f0094d8780794f6d0df2
James Hardwick <jamesdh@gmail.com>
2020-10-15 13:07:47 -0500

    Upgrade to storybook 6.0.X

--- .storybook/addons.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-actions/register'
-import '@storybook/addon-links/register'
-import '@storybook/addon-storysource/register'
