38d76597056bfeeb78bc231c29fc2a01d7519b3b
Neil Kistner <neil.kistner@gmail.com>
2020-10-20 08:15:43 -0500

    Make x/y optional

--- packages/bump/index.d.ts
+++ packages/bump/index.d.ts
@@ -14,8 +14,8 @@ declare module '@nivo/bump' {
     type SerieDerivedProp<Serie, T> = (serie: Serie) => T
 
     export interface BumpInputDatum {
-        x: string | number | null
-        y: string | number | null
+        x?: string | number | null
+        y?: string | number | null
         [key: string]: any
     }
 
