919c8d810913ad3b6d9bd6eb44514b4d72c365fd
plouc <benitteraphael@gmail.com>
2020-11-04 10:04:44 +0900

    feat(pie): add tests for legends

--- website/src/pages/pie/index.js
+++ website/src/pages/pie/index.js
@@ -76,10 +76,15 @@ const initialProperties = {
         {
             anchor: 'bottom',
             direction: 'row',
+            justify: false,
+            translateX: 0,
             translateY: 56,
+            itemsSpacing: 0,
             itemWidth: 100,
             itemHeight: 18,
             itemTextColor: '#999',
+            itemDirection: 'left-to-right',
+            itemOpacity: 1,
             symbolSize: 18,
             symbolShape: 'circle',
             effects: [
