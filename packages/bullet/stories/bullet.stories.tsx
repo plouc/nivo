23321e35b1b4eb832dcf91bf952db516f558eb82
Neil Kistner <neil.kistner@gmail.com>
2020-11-06 17:13:36 -0600

    fix(bullet): fix linting errors round 2

--- packages/bullet/stories/bullet.stories.tsx
+++ packages/bullet/stories/bullet.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+/* tslint:disable no-implicit-dependencies */
 import { storiesOf } from '@storybook/react'
 import { generateBulletData } from '@nivo/generators'
+/* tslint:enable no-implicit-dependencies */
 import { Bullet } from '../src'
 
 const data = [
