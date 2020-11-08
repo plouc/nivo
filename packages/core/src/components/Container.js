32ff2af0fa687accab93acf0dd463b10fc2497a0
Neil Kistner <neil.kistner@gmail.com>
2020-11-08 12:40:29 -0600

    refactor(core): update Container implementation to match withContainer

--- packages/core/src/components/Container.js
+++ packages/core/src/components/Container.js
@@ -6,117 +6,91 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import React, { useRef, useState, useCallback } from 'react'
+import React, { useRef, useMemo, useCallback } from 'react'
 import PropTypes from 'prop-types'
-import { tooltipContext } from '@nivo/tooltip'
+import { tooltipContext, useTooltipHandlers, TooltipWrapper } from '@nivo/tooltip'
 import noop from '../lib/noop'
-import { themeContext } from '../theming'
+import { ThemeProvider } from '../theming'
 import { MotionConfigProvider } from '../motion'
+import ConditionalWrapper from './ConditionalWrapper'
 
 const containerStyle = {
     position: 'relative',
-}
-
-const tooltipStyle = {
-    pointerEvents: 'none',
-    position: 'absolute',
-    zIndex: 10,
 }
 
 const Container = ({
     children,
     theme,
     isInteractive = true,
+    renderWrapper = true,
     animate,
     motionStiffness,
     motionDamping,
     motionConfig,
 }) => {
-    const containerEl = useRef(null)
-    const [state, setState] = useState({
-        isTooltipVisible: false,
-        tooltipContent: null,
-        position: {},
-    })
-    const showTooltip = useCallback(
-        (content, event) => {
-            if (!containerEl) return
+    const container = useRef(null)
+    const {
+        showTooltipAt,
+        showTooltipFromEvent,
+        hideTooltip,
+        isTooltipVisible,
+        tooltipContent,
+        tooltipPosition,
+        tooltipAnchor,
+    } = useTooltipHandlers(container)
 
-            const bounds = containerEl.current.getBoundingClientRect()
+    const showTooltip = useCallback((content, event) => showTooltipFromEvent(content, event), [
+        showTooltipFromEvent,
+    ])
 
-            const { clientX, clientY } = event
-
-            const x = clientX - bounds.left
-            const y = clientY - bounds.top
-
-            const position = {}
-
-            if (x < bounds.width / 2) position.left = x + 20
-            else position.right = bounds.width - x + 20
-
-            if (y < bounds.height / 2) position.top = y - 12
-            else position.bottom = bounds.height - y - 12
-
-            setState({
-                isTooltipVisible: true,
-                tooltipContent: content,
-                position,
-            })
-        },
-        [containerEl]
-    )
-    const hideTooltip = useCallback(() => {
-        setState({ isTooltipVisible: false, tooltipContent: null })
-    })
-    const { isTooltipVisible, tooltipContent, position } = state
-
-    let content
-    if (isInteractive === true) {
-        content = (
-            <div style={containerStyle} ref={containerEl}>
-                {children({
-                    showTooltip: isInteractive ? showTooltip : noop,
-                    hideTooltip: isInteractive ? hideTooltip : noop,
-                })}
-                {isTooltipVisible && (
-                    <div
-                        style={{
-                            ...tooltipStyle,
-                            ...position,
-                            ...theme.tooltip,
-                        }}
-                    >
-                        {tooltipContent}
-                    </div>
-                )}
-            </div>
-        )
-    } else {
-        content = children({
+    const handlers = useMemo(
+        () => ({
             showTooltip: isInteractive ? showTooltip : noop,
             hideTooltip: isInteractive ? hideTooltip : noop,
-        })
-    }
+        }),
+        [hideTooltip, isInteractive, showTooltip]
+    )
 
     return (
-        <themeContext.Provider value={theme}>
+        <ThemeProvider theme={theme}>
             <MotionConfigProvider
                 animate={animate}
                 stiffness={motionStiffness}
                 damping={motionDamping}
                 config={motionConfig}
             >
-                <tooltipContext.Provider value={[showTooltip, hideTooltip]}>
-                    {content}
+                <tooltipContext.Provider
+                    value={{ showTooltipAt, showTooltipFromEvent, hideTooltip }}
+                >
+                    {/* we should not render the div element if using the HTTP API */}
+                    <ConditionalWrapper
+                        condition={renderWrapper}
+                        wrapper={children => (
+                            <div style={containerStyle} ref={container}>
+                                {children}
+                                {isTooltipVisible && (
+                                    <TooltipWrapper
+                                        position={tooltipPosition}
+                                        anchor={tooltipAnchor}
+                                    >
+                                        {tooltipContent}
+                                    </TooltipWrapper>
+                                )}
+                            </div>
+                        )}
+                    >
+                        {typeof children === 'function' ? children(handlers) : children}
+                    </ConditionalWrapper>
                 </tooltipContext.Provider>
             </MotionConfigProvider>
-        </themeContext.Provider>
+        </ThemeProvider>
     )
 }
 
 Container.propTypes = {
-    children: PropTypes.func.isRequired,
+    children: PropTypes.oneOf([PropTypes.func, PropTypes.node]).isRequired,
     isInteractive: PropTypes.bool,
+    renderWrapper: PropTypes.bool,
     theme: PropTypes.object.isRequired,
     animate: PropTypes.bool.isRequired,
     motionStiffness: PropTypes.number,
