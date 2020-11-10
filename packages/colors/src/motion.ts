43465401ac4b2dfe87c8d8109a247eab7cc79138
plouc <benitteraphael@gmail.com>
2020-11-11 06:50:47 +0900

    feat(colors): migrate motion to TypeScript

--- packages/colors/src/motion.ts
+++ packages/colors/src/motion.ts
@@ -1,10 +1,12 @@
-import { spring } from 'react-motion'
+import { spring, SpringHelperConfig } from 'react-motion'
 import { rgb } from 'd3-color'
 
 /**
  * Decompose a color to be used with react-motion.
+ *
+ * @deprecated we should use `react-spring` which supports color interpolation natively.
  */
-export const interpolateColor = (color, springConfig) => {
+export const interpolateColor = (color: string, springConfig?: SpringHelperConfig) => {
     const colorComponents = rgb(color)
 
     if (!springConfig) {
@@ -30,8 +32,18 @@ export const interpolateColor = (color, springConfig) => {
 /**
  * Re-assemble interpolated color components,
  * should be used to assign a color after react-motion interpolation.
+ *
+ * @deprecated we should use `react-spring` which supports color interpolation natively.
  */
-export const getInterpolatedColor = ({ colorR, colorG, colorB }) =>
+export const getInterpolatedColor = ({
+    colorR,
+    colorG,
+    colorB,
+}: {
+    colorR: number
+    colorG: number
+    colorB: number
+}) =>
     `rgb(${Math.round(Math.max(colorR, 0))},${Math.round(Math.max(colorG, 0))},${Math.round(
         Math.max(colorB, 0)
     )})`
