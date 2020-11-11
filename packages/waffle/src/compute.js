62082c916012cfd69f5079eb5001916d3f7652d0
plouc <benitteraphael@gmail.com>
2020-11-11 09:58:16 +0900

    feat(waffle): init TypeScript migration

--- packages/waffle/src/compute.js
+++ packages/waffle/src/compute.js
@@ -1,12 +1,4 @@
-/*
- * This file is part of the nivo project.
- *
- * Copyright 2016-present, Raphaël Benitte.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-import range from 'lodash/range'
+import range from 'lodash.range'
 
 /**
  * Computes optimal cell size according to dimensions/layout/padding.
--- packages/waffle/src/enhance.js
+++ packages/waffle/src/enhance.js
@@ -1,11 +1,3 @@
-/*
- * This file is part of the nivo project.
- *
- * Copyright 2016-present, Raphaël Benitte.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 import { withDimensions, withTheme, withMotion, bindDefs } from '@nivo/core'
 import { getOrdinalColorScale, getInheritedColorGenerator } from '@nivo/colors'
 import { compose, defaultProps, withPropsOnChange, withState, pure } from '@nivo/recompose'
