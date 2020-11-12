ce5b67a10a42bf8b1be9735ec1823cfaf75d1e7b
Alexandre S <bobylito@gmail.com>
2020-11-12 17:09:55 +0100

    feat(BarCanvas): add a ref to access the canvas

--- packages/bar/src/BarCanvas.js
+++ packages/bar/src/BarCanvas.js
@@ -260,7 +260,7 @@ class BarCanvas extends Component {
     }
 
     render() {
-        const { outerWidth, outerHeight, pixelRatio, isInteractive, theme } = this.props
+        const { outerWidth, outerHeight, pixelRatio, isInteractive, theme, ref } = this.props
 
         return (
             <Container isInteractive={isInteractive} theme={theme} animate={false}>
@@ -268,6 +268,7 @@ class BarCanvas extends Component {
                     <canvas
                         ref={surface => {
                             this.surface = surface
+                            if (ref) ref.current = surface
                         }}
                         width={outerWidth * pixelRatio}
                         height={outerHeight * pixelRatio}
