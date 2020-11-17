96ac069fb14b7a2488fdc8d98eb6379056b9103a
Neil Kistner <neil.kistner@gmail.com>
2020-11-16 20:40:28 -0600

    fix(sunburst): fix with child color modifier story

--- packages/sunburst/stories/sunburst.stories.js
+++ packages/sunburst/stories/sunburst.stories.js
@@ -21,7 +21,10 @@ stories.addDecorator(withKnobs)
 stories.add('default', () => <Sunburst {...commonProperties} />)
 
 stories.add('with child color modifier', () => (
-    <Sunburst {...commonProperties} childColor="inherit:brighter(0.13)" />
+    <Sunburst
+        {...commonProperties}
+        childColor={{ from: 'color', modifiers: [['brighter', 0.13]] }}
+    />
 ))
 
 stories.add('with child colors independent of parent', () => (
