f0d9d19cb966e7181a1848fc4cca0f6d39af80b0
plouc <benitteraphael@gmail.com>
2020-12-15 13:37:27 +0900

    feat(arcs): move canvas helpers to scoped directories

--- packages/arcs/src/arc_link_labels/canvas.ts
+++ packages/arcs/src/arc_link_labels/canvas.ts
@@ -3,24 +3,8 @@ import {
     textPropsByEngine,
     CompleteTheme,
 } from '@nivo/core'
-import { DatumWithArcAndColor } from './types'
-import { ArcLabel } from './arc_labels'
-import { ArcLinkLabel } from './links'
-
-export const drawCanvasArcLabels = <Datum extends DatumWithArcAndColor>(
-    ctx: CanvasRenderingContext2D,
-    labels: ArcLabel<Datum>[],
-    theme: CompleteTheme
-) => {
-    ctx.textAlign = 'center'
-    ctx.textBaseline = 'middle'
-    ctx.font = `${theme.labels.text.fontSize}px ${theme.labels.text.fontFamily}`
-
-    labels.forEach(label => {
-        ctx.fillStyle = label.textColor
-        ctx.fillText(`${label.label}`, label.x, label.y)
-    })
-}
+import { DatumWithArcAndColor } from '../types'
+import { ArcLinkLabel } from '../links'
 
 export const drawCanvasArcLinkLabels = <Datum extends DatumWithArcAndColor>(
     ctx: CanvasRenderingContext2D,
