8ec8b1be7f977b783e253a12d71830847894d913
plouc <benitteraphael@gmail.com>
2020-12-24 11:11:11 +0900

    feat(voronoi): migrate package to TypeScript and remove recompose

---
+++ packages/voronoi/src/types.ts
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { Theme, Box } from '@nivo/core'
+import { Delaunay, Voronoi } from 'd3-delaunay'
+
+export type VoronoiDatum = {
+    id: string | number
+    x: number
+    y: number
+}
+
+export type VoronoiDomain = [number, number]
+
+export type VoronoiLayerId = 'links' | 'cells' | 'points' | 'bounds'
+
+export interface VoronoiCustomLayerProps {
+    points: {
+        x: number
+        y: number
+        data: VoronoiDatum
+    }[]
+    delaunay: Delaunay<Delaunay.Point>
+    voronoi: Voronoi<Delaunay.Point>
+}
+
+export type VoronoiCustomLayer = React.FC<VoronoiCustomLayerProps>
+
+export type VoronoiLayer = VoronoiLayerId | VoronoiCustomLayer
+
+export type VoronoiCommonProps = {
+    data: VoronoiDatum[]
+    width: number
+    height: number
+    margin?: Box
+    xDomain: VoronoiDomain
+    yDomain: VoronoiDomain
+    layers: VoronoiLayer[]
+    theme?: Theme
+    enableLinks: boolean
+    linkLineWidth: number
+    linkLineColor: string
+    enableCells: boolean
+    cellLineWidth: number
+    cellLineColor: string
+    enablePoints: boolean
+    pointSize: number
+    pointColor: string
+    role: string
+}
+
+export type VoronoiSvgProps = VoronoiCommonProps
--- packages/voronoi/tests/computeMesh.test.ts
+++ packages/voronoi/tests/computeMesh.test.ts
@@ -1,7 +1,7 @@
-import { computeMesh } from '../src/computeMesh'
+import { computeMesh } from '../src'
 
 it(`should be able to compute mesh for collinear points`, () => {
-    const points = [
+    const points: [number, number][] = [
         [0, 0],
         [50, 50],
         [100, 100],
@@ -9,15 +9,15 @@ it(`should be able to compute mesh for collinear points`, () => {
 
     const { voronoi } = computeMesh({ points, width: 500, height: 500, debug: true })
 
-    const cells = [...voronoi.cellPolygons()]
+    const cells = Array.from(voronoi!.cellPolygons())
     expect(cells).toHaveLength(3)
 })
 
 it(`should be able to compute mesh for a single point`, () => {
-    const points = [[50, 50]]
+    const points: [number, number][] = [[50, 50]]
 
     const { voronoi } = computeMesh({ points, width: 500, height: 500, debug: true })
 
-    const cells = [...voronoi.cellPolygons()]
+    const cells = Array.from(voronoi!.cellPolygons())
     expect(cells).toHaveLength(1)
 })
