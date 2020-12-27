f558a7db149aa122fec20373e2417738eeea4837
plouc <benitteraphael@gmail.com>
2020-12-27 12:48:40 +0900

    feat(scales): remove license headers

--- packages/scales/src/timeScale.js
+++ packages/scales/src/timeScale.js
@@ -1,11 +1,3 @@
-/*
- * This file is part of the nivo project.
- *
- * Copyright 2016-present, Raphaël Benitte.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 import { scaleTime, scaleUtc } from 'd3-scale'
 import PropTypes from 'prop-types'
 import { createDateNormalizer, timePrecisions, TIME_PRECISION_MILLISECOND } from './timeHelpers'
