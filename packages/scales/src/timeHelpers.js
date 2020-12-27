de4270ed2c1f9c61898669557f437d03d3fa4e3b
plouc <benitteraphael@gmail.com>
2020-12-27 12:48:40 +0900

    feat(scales): remove license headers

--- packages/scales/src/timeHelpers.js
+++ packages/scales/src/timeHelpers.js
@@ -1,11 +1,3 @@
-/*
- * This file is part of the nivo project.
- *
- * Copyright 2016-present, Raphaël Benitte.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 import { timeParse, utcParse } from 'd3-time-format'
 
 export const TIME_PRECISION_MILLISECOND = 'millisecond'
