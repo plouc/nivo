7393acf559f22b59c5638f310d8382e90ef3231f
Neil Kistner <neil.kistner@gmail.com>
2021-05-13 07:18:08 -0500

    fix(scales): linear scale when stacked types

--- packages/scales/src/linearScale.ts
+++ packages/scales/src/linearScale.ts
@@ -16,27 +16,22 @@ export const createLinearScale = <Output extends NumberValue>(
 ) => {
     let minValue: NumberValue
     if (min === 'auto') {
-        minValue = stacked === true ? data.minStacked : data.min
+        minValue = stacked === true ? data.minStacked ?? 0 : data.min
     } else {
         minValue = min
     }
 
     let maxValue: NumberValue
     if (max === 'auto') {
-        maxValue = stacked === true ? data.maxStacked : data.max
+        maxValue = stacked === true ? data.maxStacked ?? 0 : data.max
     } else {
         maxValue = max
     }
 
     const scale = scaleLinear<number, Output>()
         .rangeRound(axis === 'x' ? [0, size] : [size, 0])
+        .domain(reverse ? [maxValue, minValue] : [minValue, maxValue])
         .clamp(clamp)
-
-    if (reverse === true) {
-        scale.domain([maxValue, minValue])
-    } else {
-        scale.domain([minValue, maxValue])
-    }
 
     if (nice === true) scale.nice()
     else if (typeof nice === 'number') scale.nice(nice)
