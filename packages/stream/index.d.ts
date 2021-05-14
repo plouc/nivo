73a0812c670fbd5ffb04f386aa6a14c63fdc49cd
Neil Kistner <neil.kistner@gmail.com>
2021-05-14 09:04:29 -0500

    fix(stream): get working with scales updates

--- packages/stream/index.d.ts
+++ packages/stream/index.d.ts
@@ -20,7 +20,7 @@ import {
 } from '@nivo/core'
 import { OrdinalColorScaleConfig, InheritedColorConfig } from '@nivo/colors'
 import { LegendProps } from '@nivo/legends'
-import { Scale } from '@nivo/scales'
+import { ScaleSpec } from '@nivo/scales'
 import { AxisProps } from '@nivo/axes'
 
 declare module '@nivo/stream' {
@@ -73,8 +73,8 @@ declare module '@nivo/stream' {
 
     interface OptionalStreamProps<T> extends SvgDefsAndFill<T>, MotionProps {
         stack: StackFunc<T>
-        xScale: Scale
-        yScale: Scale
+        xScale: ScaleSpec
+        yScale: ScaleSpec
 
         order: StackOrder
         offsetType: StackOffset
