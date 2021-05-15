1f498405647bf1150ea2f98afb37d7b9c33c03b5
Cameron Evans <cam.abevans@gmail.com>
2021-05-15 15:45:26 -0300

    pr feedback - fix memoization breaking generic prop types

--- packages/axes/src/components/AxisTick.tsx
+++ packages/axes/src/components/AxisTick.tsx
@@ -3,47 +3,48 @@ import { animated } from 'react-spring'
 import { useTheme } from '@nivo/core'
 import { AxisTickProps, AxisValue } from '../types'
 
-export const AxisTick = memo(
-    <Value extends AxisValue>({
-        value: _value,
-        format,
-        lineX,
-        lineY,
-        onClick,
-        textBaseline,
-        textAnchor,
-        animatedProps,
-    }: AxisTickProps<Value>) => {
-        const theme = useTheme()
+const AxisTick = <Value extends AxisValue>({
+    value: _value,
+    format,
+    lineX,
+    lineY,
+    onClick,
+    textBaseline,
+    textAnchor,
+    animatedProps,
+}: AxisTickProps<Value>) => {
+    const theme = useTheme()
 
-        const value = format?.(_value) ?? _value
+    const value = format?.(_value) ?? _value
 
-        const props = useMemo(() => {
-            const style = { opacity: animatedProps.opacity }
+    const props = useMemo(() => {
+        const style = { opacity: animatedProps.opacity }
 
-            if (!onClick) {
-                return { style }
-            }
+        if (!onClick) {
+            return { style }
+        }
 
-            return {
-                style: { ...style, cursor: 'pointer' },
-                onClick: (event: React.MouseEvent<SVGGElement, MouseEvent>) =>
-                    onClick(event, value),
-            }
-        }, [animatedProps.opacity, onClick, value])
+        return {
+            style: { ...style, cursor: 'pointer' },
+            onClick: (event: React.MouseEvent<SVGGElement, MouseEvent>) => onClick(event, value),
+        }
+    }, [animatedProps.opacity, onClick, value])
 
-        return (
-            <animated.g transform={animatedProps.transform} {...props}>
-                <line x1={0} x2={lineX} y1={0} y2={lineY} style={theme.axis.ticks.line} />
-                <animated.text
-                    dominantBaseline={textBaseline}
-                    textAnchor={textAnchor}
-                    transform={animatedProps.textTransform}
-                    style={theme.axis.ticks.text}
-                >
-                    {value}
-                </animated.text>
-            </animated.g>
-        )
-    }
-)
+    return (
+        <animated.g transform={animatedProps.transform} {...props}>
+            <line x1={0} x2={lineX} y1={0} y2={lineY} style={theme.axis.ticks.line} />
+            <animated.text
+                dominantBaseline={textBaseline}
+                textAnchor={textAnchor}
+                transform={animatedProps.textTransform}
+                style={theme.axis.ticks.text}
+            >
+                {value}
+            </animated.text>
+        </animated.g>
+    )
+}
+
+const memoizedAxisTick = memo(AxisTick) as typeof AxisTick
+
+export { memoizedAxisTick as AxisTick }
