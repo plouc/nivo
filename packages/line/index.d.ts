b66ac11ac80553dc4075de90b882d85dc5c0d0b6
Neil Kistner <neil.kistner@gmail.com>
2021-05-27 08:20:46 -0500

    fix(line): correct typing for lineGenerator prop (#1549)

--- packages/line/index.d.ts
+++ packages/line/index.d.ts
@@ -69,7 +69,7 @@ declare module '@nivo/line' {
     export interface CustomLayerProps extends Omit<LineSvgProps, 'xScale' | 'yScale'> {
         innerHeight: number
         innerWidth: number
-        lineGenerator: D3Line<[number, number]>
+        lineGenerator: D3Line<Array<ComputedDatum['position']>>
         points: Point[]
         series: ComputedSerie[]
         xScale: ScaleFunc
