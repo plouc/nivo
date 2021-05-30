42cb06d7bc6d9cc58a1b8efcf15779f32c243812
Neil Kistner <neil.kistner@gmail.com>
2021-05-30 09:44:10 -0500

    fix(calendar): fix ResponsiveCalendar export

--- packages/calendar/src/ResponsiveCalendar.tsx
+++ packages/calendar/src/ResponsiveCalendar.tsx
@@ -3,10 +3,8 @@ import { Calendar } from './Calendar'
 import { CalendarSvgProps } from './types'
 import { ResponsiveWrapper } from '@nivo/core'
 
-const ResponsiveCalendar = (props: Omit<CalendarSvgProps, 'height' | 'width'>) => (
+export const ResponsiveCalendar = (props: Omit<CalendarSvgProps, 'height' | 'width'>) => (
     <ResponsiveWrapper>
         {({ width, height }) => <Calendar width={width} height={height} {...props} />}
     </ResponsiveWrapper>
 )
-
-export default ResponsiveCalendar
