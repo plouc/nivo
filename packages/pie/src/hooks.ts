8a88e8ab1f5a35f6afec3b91ba53775e04b7715e
Neil Kistner <neil.kistner@gmail.com>
2021-06-17 22:04:11 -0600

    fix(pie): correct argument type to usePie hook

--- packages/pie/src/hooks.ts
+++ packages/pie/src/hooks.ts
@@ -178,7 +178,7 @@ export const usePie = <RawDatum>({
     activeInnerRadiusOffset = defaultProps.activeInnerRadiusOffset,
     activeOuterRadiusOffset = defaultProps.activeOuterRadiusOffset,
 }: Pick<
-    CompletePieSvgProps<RawDatum>,
+    Partial<CompletePieSvgProps<RawDatum>>,
     | 'startAngle'
     | 'endAngle'
     | 'padAngle'
