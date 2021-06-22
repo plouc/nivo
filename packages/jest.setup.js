9407e26c34acb3eddf736e1be1a756d5eadac2bf
Neil Kistner <neil.kistner@gmail.com>
2021-06-22 08:15:18 -0500

    chore(deps): upgrade react to v17.0.2

--- packages/jest.setup.js
+++ packages/jest.setup.js
@@ -1,4 +1,4 @@
 const { configure } = require('enzyme')
-const Adapter = require('enzyme-adapter-react-16')
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17')
 
 configure({ adapter: new Adapter() })
