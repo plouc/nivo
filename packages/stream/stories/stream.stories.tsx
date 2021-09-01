2120f116b0d70512f6eafed22950a1d006e7c0d8
plouc <benitteraphael@gmail.com>
2021-09-01 17:18:50 +0900

    feat(stream): convert stories to new format

--- packages/stream/stories/stream.stories.tsx
+++ packages/stream/stories/stream.stories.tsx
@@ -1,9 +1,17 @@
 import range from 'lodash/range'
 import random from 'lodash/random'
-import { storiesOf } from '@storybook/react'
+import { Meta } from '@storybook/react'
 import { withKnobs, select } from '@storybook/addon-knobs'
+// @ts-ignore
 import { areaCurvePropKeys } from '@nivo/core'
+// @ts-ignore
 import { Stream } from '../src'
+
+export default {
+    component: Stream,
+    title: 'Stream',
+    decorators: [withKnobs],
+} as Meta
 
 const keys = ['Raoul', 'Josiane', 'Marcel', 'René', 'Paul', 'Jacques']
 
@@ -16,16 +24,12 @@ const commonProperties = {
         keys.reduce((layer, key) => {
             layer[key] = random(10, 200)
             return layer
-        }, {})
+        }, {} as any)
     ),
     animate: true,
 }
 
-const stories = storiesOf('Stream', module)
-
-stories.addDecorator(withKnobs)
-
-stories.add('default', () => <Stream {...commonProperties} />)
+export const Default = () => <Stream {...commonProperties} />
 
 const labelLookup = {
     al: 'Alabama',
@@ -33,18 +37,18 @@ const labelLookup = {
     nv: 'Nevada',
 }
 
-stories.add('custom label', () => (
+export const CustomLabel = () => (
     <Stream
         {...commonProperties}
         data={range(16).map(() =>
             Object.keys(labelLookup).reduce((layer, key) => {
                 layer[key] = random(10, 200)
                 return layer
-            }, {})
+            }, {} as any)
         )}
         keys={Object.keys(labelLookup)}
         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        label={d => labelLookup[d.id]}
+        label={d => labelLookup[d.id as keyof typeof labelLookup]}
         legends={[
             {
                 anchor: 'bottom-right',
@@ -66,28 +70,28 @@ stories.add('custom label', () => (
             },
         ]}
     />
-))
+)
 
-stories.add('full height (expand offset)', () => (
+export const fullHeight = () => (
     <Stream
         {...commonProperties}
         offsetType="expand"
         curve={select('curve', areaCurvePropKeys, 'catmullRom')}
     />
-))
+)
 
-stories.add('regular stacked chart', () => (
+export const regularStackedChart = () => (
     <Stream
         {...commonProperties}
         offsetType="none"
         axisLeft={{}}
         curve={select('curve', areaCurvePropKeys, 'catmullRom')}
     />
-))
+)
 
-stories.add('custom curve', () => <Stream {...commonProperties} curve="step" />)
+export const CustomCurve = () => <Stream {...commonProperties} curve="step" />
 
-stories.add('formatting values', () => (
+export const formattingValues = () => (
     <Stream
         {...commonProperties}
         valueFormat={value =>
@@ -96,4 +100,4 @@ stories.add('formatting values', () => (
             })} ₽`
         }
     />
-))
+)
