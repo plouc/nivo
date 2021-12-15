30a0a5321c43a815b17c476f85325098eb1069e4
plouc <benitteraphael@gmail.com>
2021-12-16 00:08:07 +0900

    feat(storybook): upgrade storybook and addons

---
+++ .storybook/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+        ['@babel/preset-env', { loose: true }],
+        ['@babel/preset-react', { runtime: 'automatic' }],
+        '@babel/preset-typescript',
+    ],
+}
