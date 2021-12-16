4cb85c929be17d3f690ea858334125dafadbd52d
plouc <benitteraphael@gmail.com>
2021-12-17 08:05:46 +0900

    feat(build): init support for development builds

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     presets: [
         ['@babel/preset-env', { loose: true }],
-        ['@babel/preset-react', { runtime: 'automatic' }],
+        ['@babel/preset-react', {
+            development: process.env.BABEL_ENV === "development",
+            runtime: 'automatic'
+        }],
         '@babel/preset-typescript',
     ],
 }
