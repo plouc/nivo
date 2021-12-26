425c1bbe574a559b54b7f3aa80fe5d60d5a776bc
plouc <benitteraphael@gmail.com>
2021-12-26 12:15:46 +0900

    feat(website): improve SEO

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,36 +1,42 @@
+const siteUrl = process.env.SITE_URL || 'https://nivo.rocks' // no trailing slash
+
 module.exports = {
-  siteMetadata: {
-    title: `nivo`,
-    description: `Supercharged React dataviz components, built on top of d3js.`,
-    author: `Raphaël Benitte`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `assets`,
-        path: `${__dirname}/src/assets`,
-      },
+    siteMetadata: {
+        title: `nivo`,
+        description: `Supercharged React dataviz components.`,
+        siteUrl,
+        og: {
+            siteName: 'nivo',
+            twitterCreator: '@benitteraphael',
+        },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {},
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `nivo`,
-        short_name: `nivo`,
-        start_url: `/`,
-        background_color: `#3c91e8`,
-        theme_color: `#3c91e8`,
-        display: `minimal-ui`,
-        icon: `src/assets/icons/nivo-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `assets`,
+                path: `${__dirname}/src/assets`,
+            },
+        },
+        `gatsby-plugin-image`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-styled-components`,
+            options: {},
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `nivo`,
+                short_name: `nivo`,
+                start_url: `/`,
+                background_color: `#3c91e8`,
+                theme_color: `#3c91e8`,
+                display: `minimal-ui`,
+                icon: `src/assets/icons/nivo-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+    ],
 }
