2763c4a28e5376457823b3be7940745c7f6feb8c
plouc <benitteraphael@gmail.com>
2022-01-13 08:33:30 +0900

    fix(core): ignore useMeasure hook if not in a browser context

--- packages/core/src/hooks/useMeasure.js
+++ packages/core/src/hooks/useMeasure.js
@@ -2,20 +2,30 @@ import { useRef, useState, useEffect } from 'react'
 
 export const useMeasure = () => {
     const measureRef = useRef(null)
+
     const [bounds, setBounds] = useState({
         left: 0,
         top: 0,
         width: 0,
         height: 0,
     })
-    const [observer] = useState(() => new ResizeObserver(([entry]) => setBounds(entry.contentRect)))
+
+    const [observer] = useState(() => {
+        // Check if window is defined (so if in the browser or in node.js).
+        const isBrowser = typeof window !== 'undefined'
+        if (!isBrowser) return null
+
+        return new ResizeObserver(([entry]) => setBounds(entry.contentRect))
+    })
 
     useEffect(() => {
-        if (measureRef.current) {
+        if (measureRef.current && observer !== null) {
             observer.observe(measureRef.current)
         }
 
-        return () => observer.disconnect()
+        return () => {
+            if (observer !== null) observer.disconnect()
+        }
     }, [])
 
     return [measureRef, bounds]
