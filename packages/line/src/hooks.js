23a5783b440c2ccf624692af851e7cf0d8735190
plouc <benitteraphael@gmail.com>
2022-02-19 06:51:16 +0900

    chore(line): remove old license headers

--- packages/line/src/hooks.js
+++ packages/line/src/hooks.js
@@ -1,11 +1,3 @@
-/*
- * This file is part of the nivo project.
- *
- * Copyright 2016-present, Raphaël Benitte.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 import { useCallback, useMemo, useState } from 'react'
 import { area, line } from 'd3-shape'
 import { curveFromProp, useTheme, useValueFormatter } from '@nivo/core'
