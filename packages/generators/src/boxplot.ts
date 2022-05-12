1e1db1731ce841e8474ed5bd1586ba6481b02161
tkonopka <tokonopka@gmail.com>
2022-05-12 15:00:24 +0100

    resolve lint errors in @nivo/generators

--- packages/generators/src/boxplot.ts
+++ packages/generators/src/boxplot.ts
@@ -1,3 +1,6 @@
+// configuration for a set of data points
+// strings are descriptors for the set
+// numbers are used for synthetic data generation)
 type BoxPlotConfig = {
     group: string
     subgroup?: string
@@ -9,18 +12,19 @@ type BoxPlotConfig = {
 // create a random number from a distribution that has a mean and some spread
 // (uses Math.atanh instead of normal distribution - only meant for test cases)
 const randomValue = (mu = 0, sigma = 1) => {
-    let z = (Math.random() - 0.5) * 2
+    const z = (Math.random() - 0.5) * 2
     return Math.atanh(z) * sigma + mu
 }
 
 export const generateBoxPlotData = (config: BoxPlotConfig[]) => {
-    let result = config.map(x => {
-        let values = Array(x.n)
-            .fill(0)
-            .map(() => randomValue(x.mu, x.sd))
-        return values.map(v => {
-            return { ...x, value: v }
+    return config
+        .map(x => {
+            const values = Array(x.n)
+                .fill(0)
+                .map(() => randomValue(x.mu, x.sd))
+            return values.map(v => {
+                return { ...x, value: v }
+            })
         })
-    })
-    return result.flat()
+        .flat()
 }
