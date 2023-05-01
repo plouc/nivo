d201a4875112d6aac664a336ef0abae312d09cc0
plouc <benitteraphael@gmail.com>
2023-05-01 14:52:56 +0900

    feat(e2e): add another test for BarCanvas

---
+++ cypress/src/components/bar/BarCanvas.cy.tsx
@@ -0,0 +1,17 @@
+import { BarCanvas } from '@nivo/bar'
+
+describe('BarCanvas', () => {
+    it('should render a bar chart', () => {
+        cy.mount(
+            <BarCanvas
+                width={500}
+                height={300}
+                data={[
+                    { id: 'one', value: 10 },
+                    { id: 'two', value: 20 },
+                    { id: 'three', value: 30 },
+                ]}
+            />
+        )
+    })
+})
