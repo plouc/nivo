b7652d180e44af0ff90f7897a08dc901f7d053de
plouc <benitteraphael@gmail.com>
2023-05-03 15:15:00 +0900

    feat(storybook): improve globl styles

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,3 +1,5 @@
+import './global.css'
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
     parameters: {
---
+++ storybook/stories/nivo-theme.ts
@@ -0,0 +1,5 @@
+import { Theme } from '@nivo/core'
+
+export const nivoTheme: Theme = {
+    fontFamily: `'SFMono-Regular', Consolas, 'Liberation Mono', Menlo, Courier, monospace`,
+}
