bca34e46b75792a2bc1865088f5470198bc3d632
plouc <benitteraphael@gmail.com>
2024-05-01 13:41:45 +0900

    feat(recompose): completely remove recompose from the project

--- packages/recompose/src/wrapDisplayName.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentType } from 'react'
-import { getDisplayName } from './getDisplayName'
-
-export const wrapDisplayName = (BaseComponent: ComponentType<any>, hocName: string): string =>
-    `${hocName}(${getDisplayName(BaseComponent)})`
