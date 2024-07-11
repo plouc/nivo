d75f70ad6491ed90b84e65e32c6df1234345d047
Carlos Catalan <ccatalan6@gatech.edu>
2024-07-11 23:34:11 +0800

    add to documentation

--- website/src/data/components/line/props.ts
+++ website/src/data/components/line/props.ts
@@ -542,6 +542,15 @@ const props: ChartProperty[] = [
         control: { type: 'switch' },
     },
     {
+        key: 'initialHiddenIds',
+        flavors: allFlavors,
+        group: 'Interactivity',
+        help: `Hides certain series by default given their ids`,
+        type: 'boolean',
+        defaultValue: defaults.initialHiddenIds,
+        control: { type: 'switch' },
+    },
+    {
         key: 'crosshairType',
         flavors: ['svg'],
         group: 'Interactivity',
