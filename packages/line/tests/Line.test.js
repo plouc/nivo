c5b78c0f1bf611841a396e9d8579e79837db78dd
Maen Juganaikloo <mjuganaikloo@gmail.com>
2024-10-09 15:56:39 +0200

    feat(test): added test for marker legend node

--- packages/line/tests/Line.test.js
+++ packages/line/tests/Line.test.js
@@ -292,6 +292,36 @@ it('should call the custom label callback for each point', () => {
     }
 })
 
+it('should display a custom legendNode for marker', () => {
+    const data = [
+        {
+            id: 'A',
+            data: [
+                { x: 0, y: 3 },
+                { x: 1, y: 7 },
+                { x: 2, y: 11 },
+                { x: 3, y: 9 },
+                { x: 4, y: 8 },
+            ],
+        },
+    ]
+    const markers = [{
+        axis: "x",
+        lineStyle: {
+            stroke: "lightblue",
+            strokeWidth: 5
+        },
+        legendPosition: "top",
+        legendNode: <foreignObject x={0} y={0} width={32} height={32}><div>😎</div></foreignObject>,
+    }]
+
+    const component = renderer.create(<Line width={500} height={300} data={data} animate={false} markers={markers} />)
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+
+})
+
 describe('curve interpolation', () => {
     const curveInterpolations = [
         'basis',
