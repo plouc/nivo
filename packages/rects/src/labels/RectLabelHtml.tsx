a35694c4bdde825d996aec29a1050a5f5ba2f6ae
plouc <benitteraphael@gmail.com>
2025-05-07 09:05:39 +0900

    feat(rects): mimic the svg labels layout for hmtl ones

--- packages/rects/src/labels/RectLabelHtml.tsx
+++ packages/rects/src/labels/RectLabelHtml.tsx
@@ -1,7 +1,14 @@
 import { animated } from '@react-spring/web'
-import { useTheme, sanitizeHtmlTextStyle } from '@nivo/theming'
+import { useTheme, sanitizeHtmlTextStyle, TextAlign, TextBaseline } from '@nivo/theming'
 import { DatumWithRectAndColor } from '../types'
 import { RectLabelProps } from './types'
+
+const getTranslation = (align: TextAlign, baseline: TextBaseline) => {
+    const translateX = align === 'start' ? '0%' : align === 'center' ? '-50%' : '-100%'
+    const translateY = baseline === 'top' ? '0%' : baseline === 'center' ? '-50%' : '-100%'
+
+    return `translate(${translateX}, ${translateY})`
+}
 
 export const RectLabelHtml = <Datum extends DatumWithRectAndColor>({
     label,
@@ -16,11 +23,19 @@ export const RectLabelHtml = <Datum extends DatumWithRectAndColor>({
             style={{
                 ...sanitizeHtmlTextStyle(theme.labels.text),
                 position: 'absolute',
-                top: style.y,
-                left: style.x,
-                opacity: style.progress,
-                pointerEvents: 'none',
+                display: 'inline-block',
+                whiteSpace: 'nowrap',
                 color,
+                left: style.x,
+                top: style.y,
+                transform: style.rotation.to(
+                    rotation =>
+                        `rotate(${rotation}deg) ${getTranslation(style.align, style.baseline)}`
+                ),
+                transformOrigin: '0 0',
+                pointerEvents: 'none',
+                lineHeight: '1em',
+                opacity: style.progress,
             }}
             data-testid={testId}
         >
