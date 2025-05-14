b937002afbc11809c6de7dfa04b15ff2115b0c57
plouc <benitteraphael@gmail.com>
2025-05-14 15:24:53 +0900

    feat(funnel): improve responsive component and add support for ref

--- packages/funnel/src/ResponsiveFunnel.tsx
+++ packages/funnel/src/ResponsiveFunnel.tsx
@@ -1,11 +1,30 @@
-import { ResponsiveWrapper } from '@nivo/core'
+import { forwardRef, Ref, ReactElement } from 'react'
+import { ResponsiveProps, ResponsiveWrapper, WithChartRef } from '@nivo/core'
 import { FunnelDatum, FunnelSvgProps } from './types'
 import { Funnel } from './Funnel'
 
-export const ResponsiveFunnel = <D extends FunnelDatum = FunnelDatum>(
-    props: Omit<FunnelSvgProps<D>, 'height' | 'width'>
-) => (
-    <ResponsiveWrapper>
-        {({ width, height }) => <Funnel<D> width={width} height={height} {...props} />}
-    </ResponsiveWrapper>
-)
+export const ResponsiveFunnel = forwardRef(
+    <D extends FunnelDatum = FunnelDatum>(
+        {
+            defaultWidth,
+            defaultHeight,
+            onResize,
+            debounceResize,
+            ...props
+        }: ResponsiveProps<FunnelSvgProps<D>>,
+        ref: Ref<SVGSVGElement>
+    ) => (
+        <ResponsiveWrapper
+            defaultWidth={defaultWidth}
+            defaultHeight={defaultHeight}
+            onResize={onResize}
+            debounceResize={debounceResize}
+        >
+            {({ width, height }) => (
+                <Funnel<D> width={width} height={height} {...props} ref={ref} />
+            )}
+        </ResponsiveWrapper>
+    )
+) as <D extends FunnelDatum = FunnelDatum>(
+    props: WithChartRef<ResponsiveProps<FunnelSvgProps<D>>, SVGSVGElement>
+) => ReactElement
