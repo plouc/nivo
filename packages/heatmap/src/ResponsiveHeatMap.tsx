dab4637d487e64645f52931331cd2dc4b78fa6ce
plouc <benitteraphael@gmail.com>
2025-05-14 16:02:35 +0900

    feat(heatmap): improve responsive component and add support for ref

--- packages/heatmap/src/ResponsiveHeatMap.tsx
+++ packages/heatmap/src/ResponsiveHeatMap.tsx
@@ -1,16 +1,36 @@
-import { ResponsiveWrapper } from '@nivo/core'
+import { forwardRef, Ref, ReactElement } from 'react'
+import { ResponsiveWrapper, WithChartRef, ResponsiveProps } from '@nivo/core'
 import { DefaultHeatMapDatum, HeatMapDatum, HeatMapSvgProps } from './types'
 import { HeatMap } from './HeatMap'
 
-export const ResponsiveHeatMap = <
+export const ResponsiveHeatMap = forwardRef(
+    <
+        Datum extends HeatMapDatum = DefaultHeatMapDatum,
+        ExtraProps extends object = Record<string, never>,
+    >(
+        {
+            defaultWidth,
+            defaultHeight,
+            onResize,
+            debounceResize,
+            ...props
+        }: ResponsiveProps<HeatMapSvgProps<Datum, ExtraProps>>,
+        ref: Ref<SVGSVGElement>
+    ) => (
+        <ResponsiveWrapper
+            defaultWidth={defaultWidth}
+            defaultHeight={defaultHeight}
+            onResize={onResize}
+            debounceResize={debounceResize}
+        >
+            {({ width, height }) => (
+                <HeatMap<Datum, ExtraProps> width={width} height={height} {...props} ref={ref} />
+            )}
+        </ResponsiveWrapper>
+    )
+) as <
     Datum extends HeatMapDatum = DefaultHeatMapDatum,
     ExtraProps extends object = Record<string, never>,
 >(
-    props: Omit<HeatMapSvgProps<Datum, ExtraProps>, 'height' | 'width'>
-) => (
-    <ResponsiveWrapper>
-        {({ width, height }) => (
-            <HeatMap<Datum, ExtraProps> width={width} height={height} {...props} />
-        )}
-    </ResponsiveWrapper>
-)
+    props: WithChartRef<ResponsiveProps<HeatMapSvgProps<Datum, ExtraProps>>, SVGSVGElement>
+) => ReactElement
